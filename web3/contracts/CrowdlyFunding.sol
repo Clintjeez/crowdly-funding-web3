8dbefb03eecdadb0a16310eba6ccb27f56080ef5
Clinton James <clintjeez@gmail.com>
2023-01-14 23:43:40 +0100

    smart contract done

--- web3/contracts/CrowdlyFunding.sol
+++ web3/contracts/CrowdlyFunding.sol
@@ -18,7 +18,7 @@ contract CrowdlyFunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(address _owner, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
     // testing
@@ -49,7 +49,18 @@ contract CrowdlyFunding {
         }
     }
 
-    function getDonators() {}
+    function getDonators(uint256 _id)  view public returns (address[] memory, uint256[] memory){
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
 
-    function getCampaigns() {}
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns); 
+
+        for(uint i = 0; i < numberOfCampaigns; i++){
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 }
