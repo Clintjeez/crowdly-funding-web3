1515d771a209da5a30917383638605921923f208
Clinton James <clintjeez@gmail.com>
2023-01-14 13:32:15 +0100

    donate campaign contract done

--- web3/contracts/CrowdlyFunding.sol
+++ web3/contracts/CrowdlyFunding.sol
@@ -18,9 +18,36 @@ contract CrowdlyFunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign() {}
+    function createCampaign(address _owner, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
 
-    function donateToCampaign() {}
+    // testing
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+     numberOfCampaigns++;
+
+     return numberOfCampaigns - 1;  
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+        Campaign.donations.push(amount);
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent){
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
 
     function getDonators() {}
 
