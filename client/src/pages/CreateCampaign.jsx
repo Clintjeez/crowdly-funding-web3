61b3be4491f00b537cec86821cd87c7e7f54187e
Clinton James <clintjeez@gmail.com>
2023-01-18 23:07:03 +0100

    routes created

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+
+import { money } from "../assets";
+import { CustomButton } from "../components";
+import { checkIfImage } from "../utils";
 
 const CreateCampaign = () => {
-  return <div>CreateCampaign</div>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    title: "",
+    description: "",
+    target: "",
+    deadline: "",
+    image: "",
+  });
+
+  return (
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+      {isLoading && "Loading..."}
+      <div>
+        <h1>Start a Campaign</h1>
+      </div>
+    </div>
+  );
 };
 
 export default CreateCampaign;
