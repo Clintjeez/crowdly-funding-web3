9690049c42bbbc5a764ed1553888dd40372c7aa6
Clinton James <clintjeez@gmail.com>
2023-01-19 00:02:41 +0100

    formfield component created

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { ethers } from "ethers";
 
 import { money } from "../assets";
-import { CustomButton } from "../components";
+import { CustomButton, FormField } from "../components";
 import { checkIfImage } from "../utils";
 
 const CreateCampaign = () => {
@@ -18,12 +18,39 @@ const CreateCampaign = () => {
     image: "",
   });
 
+  const handleSubmit = () => {};
+
   return (
     <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
       {isLoading && "Loading..."}
-      <div>
-        <h1>Start a Campaign</h1>
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a4a] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
+          Start a Campaign
+        </h1>
       </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="w-full mt-[65px] flex flex-col gap-[30px]"
+      >
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField
+            LabelName="Your Name *"
+            placeholder="John Doe"
+            inputType="text"
+            value={form.name}
+            handleChange={() => {}}
+          />
+
+          <FormField
+            LabelName="Campaign Title *"
+            placeholder="Write a title"
+            inputType="text"
+            value={form.title}
+            handleChange={() => {}}
+          />
+        </div>
+      </form>
     </div>
   );
 };
